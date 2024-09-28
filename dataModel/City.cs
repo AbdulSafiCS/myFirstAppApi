b6861c32ba3f9ea34a6c386bd135876b4d5c15f7
abdulsafics <hasibsaf90@gmail.com>
2024-09-27 17:25:03 -0700

    first database commit

---
+++ dataModel/City.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace dataModel;
+
+public partial class City
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    [Column(TypeName = "decimal(8, 6)")]
+    public decimal Lat { get; set; }
+
+    [Column(TypeName = "decimal(9, 6)")]
+    public decimal Lng { get; set; }
+
+    public int Population { get; set; }
+
+    [Column("countryID")]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
